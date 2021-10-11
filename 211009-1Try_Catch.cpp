ccb1a44e621af637873e8c6e2e5eb773ee00a5ef
areggie <mixedworldcitizen@gmail.com>
2021-10-12 00:00:25 +0300

    Wrote code of Try_Catch, Throw, ManyThrows and Catches, throwing My Exception

--- 211009-1Try_Catch.cpp
+++ 211009-1Try_Catch.cpp
@@ -6,7 +6,7 @@
 /*   By: areggie <areggie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 00:19:29 by areggie           #+#    #+#             */
-/*   Updated: 2021/10/10 00:22:00 by areggie          ###   ########.fr       */
+/*   Updated: 2021/10/11 17:55:52 by areggie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,3 +30,110 @@ try catch что это. Изучение С++ для начинающих.
 Ошибки, непредусмотренные логикой программера().
 Здесь можно использовать (Try and Catch).
 */
+
+/*
+Обработка исключений Try Catch.
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+/* ПЕРВЫЙ ВАРИАНТ КОДА*/
+
+// int main()
+// { 
+// 	setlocale (LC_ALL, "ru");
+
+// 	std::string path = "text.tx";
+	
+// 	std::ifstream fin; // создаем объект класса
+// 	// разрабы в класс ifstream добавили свои исключения:
+// 	fin.exceptions(std::ifstream::badbit | std::ifstream::failbit );
+// 	// это специфика ifstream не относится к try_catch
+
+// 	try
+// 	{
+		
+// 		// здесь пробуем работу кода 
+// 		// если все работает нормально то программа продолжает работу не заходя в секцию catch
+// 		// если что-то пошло не так то прога бросает исключит ситуацию (throw exception) в блок catch
+// 		std::cout << "Попытка открыть файл!" << std::endl;
+// 		fin.open(path); // если файл корректный, то все сработает нормально и ошибки не будет 
+// 		std::cout << "Файл успешно открыт!" << std::endl;
+		
+// 	} 
+// 	catch(const std::exception& e) //то что отловлено показывается в классе exception  в скобках
+// 	// константная ссылка на объект e типа exception
+// 	{
+// 		//блог catch отлавливает брошенное исключение
+// 		// у объекта есть метод WHAT который и показывает что пошло не так
+// 		//Этот метод возвращает строку - const char *std::exception::what() const
+// 		std::cerr << e.what() << '\n'; // в классе exception уже прописаны ошибки // но можно писать  и свой класс исключения
+// 		std::cout << "Ошибка открытия файла" << std::endl;
+
+// 		// класс exception все равно отловит метод fin.exceptions(std::ifstream::badbit | std::ifstream::failbit )
+// 		// класса ifstream так как исключения класса ifstream наследуются от общего класса exception
+		
+// 		// вместо родительского класса  (const std::exception& e) можно вызывать
+// 		// специальный классы исключений н-р (const ifstream::failure& e)
+// 		// см второй вариант кода ниже
+// 	}
+	
+
+
+// 	return 0;
+// }
+
+
+
+
+/* ВТОРОЙ ВАРИАНТ КОДА*/
+
+int main()
+{ 
+	setlocale (LC_ALL, "ru");
+
+	std::string path = "text.tx";
+	
+	std::ifstream fin; // создаем объект класса
+	// разрабы в класс ifstream добавили свои исключения:
+	fin.exceptions(std::ifstream::badbit | std::ifstream::failbit );
+	// это специфика ifstream не относится к try_catch
+	// это своего рода класс exception для ошибок ifstream
+
+	try
+	{
+		
+		// здесь пробуем работу кода 
+		// если все работает нормально то программа продолжает работу не заходя в секцию catch
+		// если что-то пошло не так то прога бросает исключит ситуацию (throw exception) в блок catch
+		std::cout << "Попытка открыть файл!" << std::endl;
+		fin.open(path); // если файл корректный, то все сработает нормально и ошибки не будет 
+		std::cout << "Файл успешно открыт!" << std::endl;
+		
+	} 
+	catch(const std::ifstream::failure& e) //то что отловлено показывается в классе exception  в скобках
+	// константная ссылка на объект e типа exception
+	{
+		//блог catch отлавливает брошенное исключение
+		// у объекта есть метод WHAT который и показывает что пошло не так
+		//Этот метод возвращает строку - const char *std::exception::what() const
+		
+		std::cerr << e.what() << '\n'; // в классе exception уже прописаны ошибки // но можно писать  и свой класс исключения
+		std::cerr << e.code() << '\n'; // у класса ifstream есть метод code: iostream:1
+		std::cout << "Ошибка открытия файла" << std::endl;
+
+		// класс exception все равно отловит метод fin.exceptions(std::ifstream::badbit | std::ifstream::failbit )
+		// класса ifstream так как исключения класса ifstream наследуются от общего класса exception
+		
+		// вместо родительского класса  (const std::exception& e) можно вызывать
+		// специальный классы исключений н-р (const ifstream::failure& e)
+		// см второй вариант кода ниже
+	}
+	
+
+
+	return 0;
+}
--- 211010-1ThrowYourException.cpp
+++ 211010-1ThrowYourException.cpp
@@ -6,26 +6,267 @@
 /*   By: areggie <areggie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 18:03:26 by areggie           #+#    #+#             */
-/*   Updated: 2021/10/10 18:04:27 by areggie          ###   ########.fr       */
+/*   Updated: 2021/10/11 22:20:37 by areggie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-
 https://www.youtube.com/watch?v=wCUl7yTHWq8
 лекция на Simple Code 123
-
 */
 
 /*
-Несколько блоков catch. 
-Обработка исключений С++. 
-Изучение С++ для начинающих. 
-Урок #123
+Свой класс exception c++. 
+Создание собственного класса исключений. 
+С++ для начинающих. Урок #123
+
 */
 
 /*
 Этот урок к CPP day05
 В продолжение лекции 120, 121, 122 о Try and Catch
 */
+
+
+/*
+создание собственных исключений
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// у класса exception есть лишь метод What()
+// поэтому можно написать свой класс исключения для бросания
+// и в нем можно прописать свои методы
+
+
+/* ПЕРВЫЙ ВАРИАНТ: кидаем свой класс MyException а ловим класс exception*/
+// прога вылетит не поймав
+// libc++abi.dylib: terminating with uncaught exception of type MyException
+// [1]    50881 abort      ./a.out
+
+// class MyException
+// {
+// 	private:
+
+// 	public:
+	
+	
+// };
+
+// void Foo (int value)
+// {
+// 	if (value < 0) 
+// 		throw std::exception(); // намеренно убрал строку "Число меньше 0!!!!!" так как на Линуксе нет конструктора
+// 		// принимающего char а на Винде есть
+
+// 	if (value == 1) 
+// 		throw MyException(); // бросаем cвой класс MyException с вызовом конструктора по умолчанию
+// }
+
+// int main()
+// { 
+// 	setlocale (LC_ALL, "ru");
+	
+// 	try
+// 	{
+// 		Foo(1); 
+	
+// 	}
+// 	catch( std::exception &excep ) //
+// 	{
+// 		std::cerr << "Блок 1 Мы поймали " << excep.what() << '\n';
+
+// 	}
+
+// 	return 0;
+// }
+
+
+/* ВТОРОЙ ВАРИАНТ: кидаем свой класс MyException и ловим свой класс MyException*/
+// прога сработает
+
+
+// class MyException
+// {
+// 	private:
+
+// 	public:
+	
+	
+// };
+
+// void Foo (int value)
+// {
+// 	if (value < 0) 
+// 		throw std::exception(); // намеренно убрал строку "Число меньше 0!!!!!" так как на Линуксе нет конструктора
+// 		// принимающего char а на Винде есть
+
+// 	if (value == 1) 
+// 		throw MyException(); // бросаем cвой класс MyException с вызовом конструктора по умолчанию
+// }
+
+// int main()
+// { 
+// 	setlocale (LC_ALL, "ru");
+	
+// 	try
+// 	{
+// 		Foo(1); 
+	
+// 	}
+// 	catch( MyException &excep ) //
+// 	{
+// 		std::cerr << "Блок 1 Мы поймали " << '\n';
+
+// 	}
+
+// 	return 0;
+// }
+
+
+
+/* ТРЕТИЙ ВАРИАНТ: кидаем свой класс MyException и ловим  базовым (родительским) классом Exception */
+// прога сработает так как MyException будет наследовать от exception
+
+
+// class MyException : public std::exception
+// {
+// 	private:
+
+// 	public:
+	
+	
+// };
+
+// void Foo (int value)
+// {
+// 	if (value < 0) 
+// 		throw std::exception(); // намеренно убрал строку "Число меньше 0!!!!!" так как на Линуксе нет конструктора
+// 		// принимающего char а на Винде есть
+
+// 	if (value == 1) 
+// 		throw MyException(); // бросаем cвой класс MyException с вызовом конструктора по умолчанию
+// }
+
+// int main()
+// { 
+// 	setlocale (LC_ALL, "ru");
+	
+// 	try
+// 	{
+// 		Foo(1); 
+	
+// 	}
+// 	catch( std::exception &excep ) // так как MyException это наследник от exception то перехват возможен
+// 	{
+// 		std::cerr << "Блок 1 Мы поймали " << '\n';
+
+// 	}
+
+// 	return 0;
+// }
+
+
+
+/* ЧЕТВЕРТЫЙ ВАРИАНТ: кидаем свой класс MyException и наследуемся от базовым (родительского) класса Exception 
+но ловим своим классом  MyException*/
+
+
+
+// class MyException : public std::exception
+// {
+// 	private:
+
+// 	public:
+	
+	
+// };
+
+// void Foo (int value)
+// {
+// 	if (value < 0) 
+// 		throw std::exception(); // намеренно убрал строку "Число меньше 0!!!!!" так как на Линуксе нет конструктора
+// 		// принимающего char а на Винде есть
+
+// 	if (value == 1) 
+// 		throw MyException(); // бросаем cвой класс MyException с вызовом конструктора по умолчанию
+// }
+
+// int main()
+// { 
+// 	setlocale (LC_ALL, "ru");
+	
+// 	try
+// 	{
+// 		Foo(1); 
+	
+// 	}
+// 	catch( MyException &excep ) // так как MyException это наследник от exception то перехват возможен
+// 	{
+// 		std::cerr << "Блок 1 Мы поймали " << excep.what() << '\n'; // вызываем унаследованный метод what
+
+// 	}
+
+// 	return 0;
+// }
+
+
+
+
+/* ПЯТЫЙ ВАРИАНТ: кидаем свой класс MyException и наследуемся от базовым (родительского) класса Exception 
+но ловим своим классом  MyException*/
+// дополнительно в свой класс можно прописать свои методы
+
+class MyException : public std::exception
+{
+	private:
+		int dataState;
+
+	public:
+		int MyException::InsideMethod(int dataState)
+		{
+			this->dataState = dataState;
+		}
+		
+		int GetDataState()
+		{
+			return dataState;
+		}
+
+		std::cout << "Переменная = " << value << std::endl;
+	
+	
+};
+
+void Foo (int value)
+{
+	if (value < 0) 
+		throw std::exception(); // намеренно убрал строку "Число меньше 0!!!!!" так как на Линуксе нет конструктора
+		// принимающего char а на Винде есть
+
+	if (value == 1) 
+		throw MyException(value); // бросаем cвой класс MyException с вызовом конструктора по умолчанию
+}
+
+int main()
+{ 
+	setlocale (LC_ALL, "ru");
+	
+	try
+	{
+		Foo(1); 
+	
+	}
+	catch( MyException &excep ) // так как MyException это наследник от exception то перехват возможен
+	{
+		std::cerr << "Блок 1 Мы поймали " << excep.what() << '\n'; // вызываем унаследованный метод what
+		std::cerr << "Состояние данных " << excep.GetDataState() << '\n'; // вызываем унаследованный метод what
+
+	}
+
+	return 0;
+}
+
 
