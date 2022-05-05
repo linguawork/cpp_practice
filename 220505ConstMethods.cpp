afe4d0c8892f1961284747c3553f840fb4bd35b9
areggie <mixedworldcitizen@gmail.com>
2022-05-05 14:40:22 +0300

    Added ConstMethods, IteratorClass samples

---
+++ 220505ConstMethods.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   220505ConstMethods.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: areggie <areggie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 14:36:15 by areggie           #+#    #+#             */
+/*   Updated: 2022/05/05 14:39:18 by areggie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	the question related to cpp exam rank 05
+	
+	What is the meaning of constant method?	
+	https://stackoverflow.com/questions/3141087/what-is-meant-with-const-at-end-of-function-declaration
+
+	All mutable data in the method is protected from modifications
+
+
+
+*/
+
+const std::string & someMethod() const;
+
+
