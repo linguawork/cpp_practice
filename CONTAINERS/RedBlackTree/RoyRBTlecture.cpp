6a7df32458c60feefd09f1e6e6fa02e7f57e5a9a
areggie <mixedworldcitizen@gmail.com>
2022-05-31 19:22:41 +0300

    Added case 4 and 6 after deleting double black

--- CONTAINERS/RedBlackTree/RoyRBTlecture.cpp
+++ CONTAINERS/RedBlackTree/RoyRBTlecture.cpp
@@ -6,14 +6,13 @@
 /*   By: areggie <areggie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:16:50 by areggie           #+#    #+#             */
-/*   Updated: 2022/05/31 15:53:27 by areggie          ###   ########.fr       */
+/*   Updated: 2022/05/31 19:21:36 by areggie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	I watched the video of Roy about RBT INSERTION https://www.youtube.com/watch?v=UaLIHuR1t8Q
 	after watching Rob's vidoes, it is kind of good time of repetition
-
 
 	I also made  time code:
 	Thank you, Mr.Tushar Roy! Clear explanation!
@@ -33,12 +32,12 @@
 	RBT deletion:
 	https://www.youtube.com/watch?v=CTvfzU_uNKE&list=PLYlfM6qKBI5indgvpVEzTlMh8sXt_I6XD&index=4
 	
-	
 	1:53 - 3:41 first case: we delete black 30, copy the smallest number of the bigger successors on the right (which has 1 or 0 not null ptrs, meaning less than 2 children (1 or 0)) in place of 30, that is 35.
 	then we go down to the source red 35 (red nodes are simply deleted attaching null).
 	3:42 - 5:28 second case: we delete black 30, copy the smallest number of the bigger successors which are on the right side of the node to be deleted (which has 1 or 0 not null ptrs, meaning less than 2 children (1 or 0)) in place of 30, that is 35 to copy.
 	then we go down to source black 35 (this time it is black and has a red successor). So delete the source black 35, put red successor on its place and make it black.)
 	6:10 there are 6 cases to delete a double black node. (6:17 - 8:25 explanation of double black node first)
-	8:26-9:08 how 6 cases of deletion work (they are applied one by one from 1 to 6 up to one of the terminal cases: 1, 4, 6)
-	
+	8:26 - 9:35 how 6 cases of deletion work (they are applied one by one from 1 to 6 up to one of the terminal cases: 1, 4, 6)
+	9:35 - 11:57 double black case 4 (easiest, change color only)
+	12:00 - 15:10 double black case 6 (before left rotation grandparent and left child can be any color, after L-rotation left child goes to left side and becomes right child of any color.) One question: how can S become any color if it should be black after transformation.	
 */
